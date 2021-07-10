bb2b78f7fc0c587a8df84fea694b32e1cdfe8c8f
Miguel Mejia <miguelmejia92@outlook.com>
2021-07-10 15:34:24 -0500

    Send message from Producer

--- src/main/java/com/migsoft/kafka/producers/JavaProducer.java
+++ src/main/java/com/migsoft/kafka/producers/JavaProducer.java
@@ -1,13 +1,24 @@
 package com.migsoft.kafka.producers;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
 import java.util.Properties;
 
 public class JavaProducer {
     public static void main(String[] args) {
         Properties props = new Properties();
         props.put("bootstrap.servers", "localhost:9092"); // Broker to connect
-        props.put("acks", "1"); // 0 - 1 - all
+        props.put("acks", "all"); // 0 - 1 - all
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer"); // key type (IntegerSerializer (ex))
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer"); // key type
+
+        try (Producer<String, String> producer = new KafkaProducer<>(props)){
+            producer.send(new ProducerRecord<>("java-topic", "java-key", "java-value"));
+        } catch (Exception e){
+
+        }
+
     }
 }
