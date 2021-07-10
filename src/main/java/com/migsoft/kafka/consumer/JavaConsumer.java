b1bb718ff7c90fe05f09836f46aed22ead070798
Miguel Mejia <miguelmejia92@outlook.com>
2021-07-10 16:47:50 -0500

    Set consumer properties

--- src/main/java/com/migsoft/kafka/consumer/JavaConsumer.java
+++ src/main/java/com/migsoft/kafka/consumer/JavaConsumer.java
@@ -1,4 +1,22 @@
 package com.migsoft.kafka.consumer;
 
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
 public class JavaConsumer {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.setProperty("bootstrap.servers","localhost:9092");
+        props.setProperty("group.id","course-group");
+        props.setProperty("enable.auto.commit","true");
+        props.setProperty("auto.commit.interval.ms","1000");
+        props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+
+    }
 }
