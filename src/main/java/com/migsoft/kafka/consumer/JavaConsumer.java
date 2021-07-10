10a68f9a0bc92e322b01fd187e9f7cb9421eff02
Miguel Mejia <miguelmejia92@outlook.com>
2021-07-10 16:54:31 -0500

    Create consumer

--- src/main/java/com/migsoft/kafka/consumer/JavaConsumer.java
+++ src/main/java/com/migsoft/kafka/consumer/JavaConsumer.java
@@ -1,13 +1,18 @@
 package com.migsoft.kafka.consumer;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
 
 public class JavaConsumer {
+    private static final Logger log = LoggerFactory.getLogger(JavaConsumer.class);
+
     public static void main(String[] args) {
         Properties props = new Properties();
         props.setProperty("bootstrap.servers","localhost:9092");
@@ -17,6 +22,16 @@ public class JavaConsumer {
         props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
+        try (KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props)){
+            consumer.subscribe(Arrays.asList("java-topic"));
+            while(true){
+                ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<String, String> consumerRecord: consumerRecords) {
+                    log.info("Offset = {}, Partition = {}, Key = {}, Value = {}", consumerRecord.offset(), consumerRecord.partition(),
+                            consumerRecord.key(), consumerRecord.value());
 
+                }
+            }
+        }
     }
 }
